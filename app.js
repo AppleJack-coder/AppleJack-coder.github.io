cd1fbe8be79733551c1802bad318d65b41912874
AppleJack-coder <mirumian999@gmail.com>
2023-04-28 19:49:23 +0300

    test: add request to another server

--- app.js
+++ app.js
@@ -2,4 +2,19 @@ let tg = window.Telegram.WebApp;
 
 tg.expand()
 
-tg.sendData('data');
+var http = new XMLHttpRequest();
+var url = "https://line05w.bk6bba-resources.com/events/list?lang=ru&version=0&scopeMarket=1600";
+var params = JSON.stringify(myJSONobj);
+http.open("GET", url, true);
+
+//Send the proper header information along with the request
+http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+http.onreadystatechange = function() {
+    if(http.readyState == 4 && http.status == 200) {
+        document.getElementById('test').innerHtml = JSON.parse(http.responseText['sports']);//the JSON string from server will show here
+    }
+}
+http.send(params);
+
+//tg.sendData('data');
